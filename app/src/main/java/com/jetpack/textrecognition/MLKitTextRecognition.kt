f582dd43c7af5ed8c8e94ace6bd96180b82bbc54
MakeItEasyDev <89842411+MakeItEasyDev@users.noreply.github.com>
2022-02-04 06:59:27 +0530

    Add files via upload

---
+++ app/src/main/java/com/jetpack/textrecognition/MLKitTextRecognition.kt
@@ -0,0 +1,165 @@
+package com.jetpack.textrecognition
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.TextRecognizer
+import java.util.concurrent.Executors
+
+@Composable
+fun MLKitTextRecognition() {
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val extractedText = remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        TextRecognitionView(
+            context = context,
+            lifecycleOwner = lifecycleOwner,
+            extractedText = extractedText
+        )
+        Text(
+            text = extractedText.value,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White)
+                .padding(16.dp)
+        )
+    }
+}
+
+@Composable
+fun TextRecognitionView(
+    context: Context,
+    lifecycleOwner: LifecycleOwner,
+    extractedText: MutableState<String>
+) {
+    val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(context) }
+    var preview by remember { mutableStateOf<Preview?>(null) }
+    val executor = ContextCompat.getMainExecutor(context)
+    val cameraProvider = cameraProviderFuture.get()
+    val textRecognizer = remember { TextRecognition.getClient() }
+    val cameraExecutor = remember { Executors.newSingleThreadExecutor() }
+
+    Box {
+        AndroidView(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.7f),
+            factory = { ctx ->
+                val previewView = PreviewView(ctx)
+                cameraProviderFuture.addListener({
+                    val imageAnalysis = ImageAnalysis.Builder()
+                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                        .build()
+                        .apply {
+                            setAnalyzer(cameraExecutor, ObjectDetectorImageAnalyzer(textRecognizer, extractedText))
+                        }
+                    val cameraSelector = CameraSelector.Builder()
+                        .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                        .build()
+                    cameraProvider.unbindAll()
+                    cameraProvider.bindToLifecycle(
+                        lifecycleOwner,
+                        cameraSelector,
+                        imageAnalysis,
+                        preview
+                    )
+                }, executor)
+                preview = Preview.Builder().build().also {
+                    it.setSurfaceProvider(previewView.surfaceProvider)
+                }
+                previewView
+            }
+        )
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(15.dp)
+                .align(Alignment.TopStart)
+        ) {
+            IconButton(
+                onClick = { Toast.makeText(context, "Back Clicked", Toast.LENGTH_SHORT).show() }
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = "back",
+                    tint = Color.White
+                )
+            }
+        }
+    }
+}
+
+class ObjectDetectorImageAnalyzer(
+    private val textRecognizer: TextRecognizer,
+    private val extractedText: MutableState<String>
+): ImageAnalysis.Analyzer {
+    @SuppressLint("UnsafeOptInUsageError")
+    override fun analyze(imageProxy: ImageProxy) {
+        val mediaImage = imageProxy.image
+        if (mediaImage != null) {
+            val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+
+            textRecognizer.process(image)
+                .addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        extractedText.value = it.result?.text ?: ""
+                    }
+                    imageProxy.close()
+                }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/jetpack/textrecognition/MainActivity.kt
@@ -0,0 +1,88 @@
+package com.jetpack.textrecognition
+
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.core.app.ActivityCompat
+import com.jetpack.textrecognition.ui.theme.TextRecognitionTheme
+
+private const val REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST = 34
+private fun foregroundPermissionApproved(context: Context): Boolean {
+    return PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(
+        context, Manifest.permission.CAMERA
+    )
+}
+
+private fun requestForegroundPermission(context: Context) {
+    val provideRationale = foregroundPermissionApproved(context)
+
+    if (provideRationale) {
+        ActivityCompat.requestPermissions(
+            context as Activity,
+            arrayOf(Manifest.permission.CAMERA), REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST
+        )
+    } else {
+        ActivityCompat.requestPermissions(
+            context as Activity,
+            arrayOf(Manifest.permission.CAMERA), REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST
+        )
+    }
+}
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TextRecognitionTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        requestForegroundPermission(this@MainActivity)
+                        TopAppBar(
+                            title = {
+                                Text(
+                                    text = "Text Recognition",
+                                    modifier = Modifier.fillMaxWidth(),
+                                    textAlign = TextAlign.Center
+                                )
+                            }
+                        )
+                        MLKitTextRecognition()
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
